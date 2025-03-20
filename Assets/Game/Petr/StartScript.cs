5c872cefc69fcef2aab7446ca9fb2423e2577ed0
PonchikPro3 <kiselevkik@yandex.ru>
2025-03-20 20:33:26 +0300

    ь

--- Assets/Game/Petr/StartScript.cs
+++ Assets/Game/Petr/StartScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class StartScript : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class StartScript : MonoBehaviour
 
     public AudioClip soundEffect;
     [SerializeField] float time_move = 0.5f;
+    [SerializeField] float fadeTime = 0.3f;
+    [SerializeField] Image darkPanel;
 
     void Start()
     {
@@ -16,12 +19,32 @@ public class StartScript : MonoBehaviour
         {
             audioSource = gameObject.AddComponent<AudioSource>();
         }
+        
+        // Initialize dark panel if not assigned
+        if (darkPanel == null)
+        {
+            Debug.LogWarning("Dark panel not assigned. Please assign a UI Image to the darkPanel field.");
+        }
+        else
+        {
+            // Make sure the panel starts transparent
+            Color panelColor = darkPanel.color;
+            panelColor.a = 0f;
+            darkPanel.color = panelColor;
+        }
     }
 
     public void Start_Button()
     {
         PlaySound(soundEffect);
         transform.DOMoveY(0, time_move);
+        
+        // Fade in the dark panel
+        if (darkPanel != null)
+        {
+            darkPanel.gameObject.SetActive(true);
+            darkPanel.DOFade(1, fadeTime);
+        }
     }
 
     public void PlaySound()
